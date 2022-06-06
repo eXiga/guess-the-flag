ad326874f62c16b042e62128993665996a378525
eXiga <anton.kostenich@hotmail.com>
2022-06-07 00:10:35 +0200

    Implement possible answers generation and basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-function App() {
-  return <h1 className="text-9xl">Hello world</h1>;
+import Answers from "./components/Answers";
+import Flag from "./components/Flag";
+import { codes } from "./data/codes";
+
+function random(min: number, max: number): number {
+  const left = Math.ceil(min);
+  const right = Math.floor(max);
+
+  return Math.floor(Math.random() * (right - left + 1)) + left;
 }
 
-export default App;
+export default function App() {
+  const countries = [
+    codes[random(0, 242)],
+    codes[random(0, 242)],
+    codes[random(0, 242)],
+    codes[random(0, 242)],
+  ];
+
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      <Flag country={countries[0]} />
+      <Answers countries={countries} />
+    </div>
+  );
+}
